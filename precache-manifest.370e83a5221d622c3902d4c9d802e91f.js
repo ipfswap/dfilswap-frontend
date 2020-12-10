11491e32c634efda0e73651068f2725354078af1
ipfswap <ipfswap@gmail.com>
2020-12-10 13:46:15 +0800

    first commit

---
+++ precache-manifest.370e83a5221d622c3902d4c9d802e91f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e5c8811b7d0ec99bfc681360e61496",
+    "url": "/dfilswap-frontend/index.html"
+  },
+  {
+    "revision": "998a97fe8aa818ff54a9",
+    "url": "/dfilswap-frontend/static/css/main.b542106b.chunk.css"
+  },
+  {
+    "revision": "970657186cb7ee1d721d",
+    "url": "/dfilswap-frontend/static/js/0.bfef713d.chunk.js"
+  },
+  {
+    "revision": "a811422eaa37f6cf16a9",
+    "url": "/dfilswap-frontend/static/js/10.970cff79.chunk.js"
+  },
+  {
+    "revision": "561234cfed784d3d5201",
+    "url": "/dfilswap-frontend/static/js/3.13f34121.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/dfilswap-frontend/static/js/3.13f34121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4f7db1a19686217ae9",
+    "url": "/dfilswap-frontend/static/js/4.695ce313.chunk.js"
+  },
+  {
+    "revision": "72069f3b1d19ef122387",
+    "url": "/dfilswap-frontend/static/js/5.5d4ccae0.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/dfilswap-frontend/static/js/5.5d4ccae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c48d2b34fb142b8102",
+    "url": "/dfilswap-frontend/static/js/6.6ccabdcb.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/dfilswap-frontend/static/js/6.6ccabdcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029197991022715192d4",
+    "url": "/dfilswap-frontend/static/js/7.f938b36b.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/dfilswap-frontend/static/js/7.f938b36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3834a758748fca84319",
+    "url": "/dfilswap-frontend/static/js/8.b983f7c6.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/dfilswap-frontend/static/js/8.b983f7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db83ad6a9d2dedaf6f5",
+    "url": "/dfilswap-frontend/static/js/9.afc41d86.chunk.js"
+  },
+  {
+    "revision": "998a97fe8aa818ff54a9",
+    "url": "/dfilswap-frontend/static/js/main.34fbae2f.chunk.js"
+  },
+  {
+    "revision": "b03ee30eb0ed8de2f9ec",
+    "url": "/dfilswap-frontend/static/js/runtime-main.7d48b266.js"
+  },
+  {
+    "revision": "8d58469f637ffb28e0f82770d68f399c",
+    "url": "/dfilswap-frontend/static/media/chef.8d58469f.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/dfilswap-frontend/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/dfilswap-frontend/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
