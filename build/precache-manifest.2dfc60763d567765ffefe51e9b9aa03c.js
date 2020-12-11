55e9fa43f054abe8bcfb267a3b2ed7cda855d89e
ipfswap <ipfswap@gmail.com>
2020-12-11 22:49:57 +0800

    first commit

---
+++ build/precache-manifest.2dfc60763d567765ffefe51e9b9aa03c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe7a6a7990e1e26e32b64bf77804375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc0dbefd47ed43448d2",
+    "url": "/static/css/main.b542106b.chunk.css"
+  },
+  {
+    "revision": "970657186cb7ee1d721d",
+    "url": "/static/js/0.bfef713d.chunk.js"
+  },
+  {
+    "revision": "a811422eaa37f6cf16a9",
+    "url": "/static/js/10.970cff79.chunk.js"
+  },
+  {
+    "revision": "df0a6daa53af23450031",
+    "url": "/static/js/3.89190b8e.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "/static/js/3.89190b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4f7db1a19686217ae9",
+    "url": "/static/js/4.695ce313.chunk.js"
+  },
+  {
+    "revision": "72069f3b1d19ef122387",
+    "url": "/static/js/5.5d4ccae0.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/5.5d4ccae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c48d2b34fb142b8102",
+    "url": "/static/js/6.6ccabdcb.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/6.6ccabdcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1b2d7d4a68f3eb7c59",
+    "url": "/static/js/7.b9593226.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/7.b9593226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3834a758748fca84319",
+    "url": "/static/js/8.b983f7c6.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.b983f7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db83ad6a9d2dedaf6f5",
+    "url": "/static/js/9.afc41d86.chunk.js"
+  },
+  {
+    "revision": "bbc0dbefd47ed43448d2",
+    "url": "/static/js/main.0d3b4e0c.chunk.js"
+  },
+  {
+    "revision": "a697966c7fe7cd0becb5",
+    "url": "/static/js/runtime-main.7cd0bb39.js"
+  },
+  {
+    "revision": "8d58469f637ffb28e0f82770d68f399c",
+    "url": "/static/media/chef.8d58469f.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9439154bba2d375737b9b4986ebad30b.js"
+  "/precache-manifest.2dfc60763d567765ffefe51e9b9aa03c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
